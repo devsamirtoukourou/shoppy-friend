7a67cb01087aefb5d88c1e8ac9f33679703f6794
mohamedtoukourou2009 <69804566+mohamedtoukourou2009@users.noreply.github.com>
2021-03-02 10:39:23 +0100

    Skeleton added

---
+++ app/src/main/java/com/example/shoppy_friend/F04_Wallet_Fragment.java
@@ -0,0 +1,16 @@
+package com.example.shoppy_friend;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class F04_Wallet_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_04_wallet, container, false);
+    }
+}
--- app/src/main/java/com/example/shoppy_friend/MainActivity.java
+++ app/src/main/java/com/example/shoppy_friend/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.shoppy_friend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     public void initUI() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F01_Shopping_List_Fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_rss);
+        }
+
     }
 
     @Override
@@ -37,5 +50,31 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_rss:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F01_Shopping_List_Fragment()).commit();
+                break;
+            case R.id.nav_my_favorites:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F02_My_favourites_Fragment()).commit();
+                break;
+            case R.id.nav_purchase_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F03_Purchase_history_Fragment()).commit();
+                break;
+            case R.id.nav_wallet:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F04_Wallet_Fragment()).commit();
+                break;
+            case R.id.nav_about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F05_About_Fragment()).commit();
+                break;
+            case R.id.nav_deconnexion:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new F06_Deconnexion_Fragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
